0beb4a5fcd89780bd4ca4b1c38aec9fe01f203c5
Rex Mingla <rexmingla@gmail.com>
2018-01-19 07:34:19 +1100

    Starting path towards multi-device support using Garmodoro (well really garmin-connect-seed)

---
+++ properties.mk
@@ -0,0 +1,6 @@
+DEVICE = fr735xt
+SDK_HOME = /Users/Tim/dev/vendor/connectiq-sdk-mac/
+DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
+#SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium d2charlie fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos fr230 fr235 fr630 fr735xt fr920xt vivoactive vivoactive_hr vivoactive3 epix edge_1000 edge_520 edge820 oregon7xx edge_1000
+SUPPORTED_DEVICES_LIST = fr735xt
+PRIVATE_KEY = /Users/Tim/dev/vendor/connectiq-sdk-mac/developer_key
